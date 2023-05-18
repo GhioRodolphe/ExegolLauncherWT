cc7cfcfe7d87bf9c05e6280c02e979f130a65b58
Rodolphe Ghio <rodolpheghio@gmail.com>
2023-05-18 15:17:22 +0200

    Improved display, improved functions names, quick refactoring

--- script.ps1
+++ script.ps1
@@ -1,92 +1,65 @@
 # Function to get containers with matching image prefix
-Function Get-MatchingContainers {
+Function Get-Container {
     param ($imagePrefix)
 
     # Get the list of containers with the matching image prefix
     $containers = docker ps -a --format "{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}"
-
+    $index = 0
     # Filter the containers by the matching image prefix
-    $matchingContainers = @()
+    return $containers | Where-Object { $_ -match "^.*$imagePrefix-.*$" } |
 
-    foreach ($container in $containers) {
-        if ($container -match "^.*$imagePrefix.*$") {
-            $matchingContainers += $container
+        ForEach-Object {
+            $containerData = $_ -split "\t"
+            New-Object PSObject -Property @{
+                Index  = $index
+                ID     = $containerData[0]
+                Image  = $containerData[1]
+                Names  = $containerData[2]
+                Status = $containerData[3]
+            }
+            $index ++
         }
+}
+
+# Function to manage a container
+Function Invoke-ContainerManagement {
+    param ($containerId, $status)
+
+    # Check if container is not running and start it
+    if ($status.Contains("Exited")) {
+        Write-Host "Starting container '$containerId'..." -ForegroundColor Green
+        docker start $containerId
     }
 
-    return $matchingContainers
-}
-
-# Function to start a container
-Function Start-Container {
-    param ($containerId)
-
-    Write-Host "Starting container '$containerId'..." -ForegroundColor Green
-    docker start $containerId
-}
-
-# Function to enter exec mode for a container
-Function Exec-Container {
-    param ($containerId)
-
-    Write-Host "Entering exec mode for container '$containerId'..." -ForegroundColor Green
+    Write-Host "Entering interactive mode for container '$containerId'..." -ForegroundColor Green
     docker exec -it $containerId /bin/zsh
 }
 
-Function Exegol-Start {
+Function Invoke-ExegolStart {
     Write-Host "Running 'exegol start' in interactive mode" -ForegroundColor Green
     exegol start
 }
 
-Function Choose-Container {
-    param ($containers)
-
-    # If no containers are available, display an error message
-    if ($null -eq $matchingContainers) {
-        Write-Host "No containers with image prefix '$imagePrefix' found." -ForegroundColor Red
-        Exegol-Start
-    }
-    else {
-        # Display the list and allow the user to choose a container
-        Write-Host "List of available containers:"
-        $index = 0
-        foreach ($container in $containers) {
-            $containerArray = $container -split "\t"
-            $containerImage = $containerArray[1]
-            $containerNames = $containerArray[2]
-            Write-Host "${index} : $containerImage`t$containerNames" -ForegroundColor Yellow
-            $index++
-        }
-
-        # Ask the user to choose a container
-        $choice = Read-Host "Choose a container index or press enter to run 'exegol start' in interactive mode "
-
-        if ($choice -eq '') {
-            Exegol-Start
-        }
-        else {
-            $choice = [int]$choice
-
-            if ($choice -ge $index) {
-                Write-Host "This container does not exist" -ForegroundColor Red
-                Choose-Container -containers $containers
-            }
-            else {
-                # Start the container if necessary and enter exec mode for the chosen container
-                $containerIdArray = $matchingContainers[$choice] -split "\t"
-                if ($containerIdArray[3].Contains("Exited")) {
-                    Start-Container -containerId $containerIdArray[0]
-                }
-                Exec-Container -containerId $containerIdArray[0]
-            }
-        }
-    }
-}
-
 # Get the prefix of the image
-$imagePrefix = "nwodtuhs/exegol"
+$imagePrefix = "exegol"
 
 # Get the matching containers
-[array]$matchingContainers = Get-MatchingContainers -imagePrefix $imagePrefix
+$matchingContainers = Get-Container -imagePrefix $imagePrefix
 
-Choose-Container -containers $matchingContainers
+# Display the list of containers or launch Exegol if no containers are found
+if ($matchingContainers.Count -eq 0) {
+    Invoke-ExegolStart
+} else {
+    Write-Host "List of available containers:" -ForegroundColor Yellow
+    $matchingContainers | Format-Table -AutoSize -Property Index, ID, Image, Names, Status
+
+    # Ask the user to choose a container
+    $choice = Read-Host "Choose a container ID or press enter to run 'exegol start' in interactive mode "
+
+    if ($choice -eq '') {
+        Invoke-ExegolStart
+    } else {
+        $chosenContainer = $matchingContainers | Where-Object { $_.Index -eq $choice }
+        Invoke-ContainerManagement -containerId $chosenContainer.ID -status $chosenContainer.Status
+    }
+}
